1c63491909f1a0d86d602fd999216e7977ba819c
Imre Borsi <borsiimre@hotmail.com>
2026-03-29 13:36:11 +0200

    Add property manager form CTA

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const links = {
   home: "https://borsiimre60.github.io/",
   kozoskepviselok: "https://borsiimre60.github.io/kozoskepviselok/",
+  kozoskepviseloForm: "https://docs.google.com/forms/d/e/1FAIpQLSfbSlrvX8jRNn0K3WF7z-yO2KEKpI40qX7tWSVVk0XNBhhh7A/viewform?usp=pp_url&entry.716565563=kozos_kepviselo_kampany",
   vendeglatohelyek: "https://borsiimre60.github.io/vendeglatohelyek/",
   szakemberek: "https://borsiimre60.github.io/szakemberek/",
   szakipush: "https://borsiimre60.github.io/szakipush-web/"
